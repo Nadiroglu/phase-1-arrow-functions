218bf4955bf8b698731118678407f966a43f36a0
Nail Nadiroglu <nadiroglunail@gmail.com>
2023-12-11 23:07:47 -0500

    arrow func

---
+++ index.js
@@ -0,0 +1,29 @@
+const divide = function(a = 2000, b = 100){
+    return a / b;
+}
+const num = [2];
+const square = x => x ** 2
+const add = (a,b) => a + b
+
+// const add = (a,b) => {
+//     console.log(add)
+// }
+// const square = function(x = 2) {
+//     return x ** 2
+// }
+// // const nums = [1,2,3];
+// // const squares = nums.map(x => x ** 2);
+// // squares();
+
+
+// finishedItems = overdueTodoItems.map( item => item.className = "complete")
+// Headers.innertexzt = `You finished ${finishedItems.length} items`;
+
+
+// function add(apple = 5) {
+
+// }
+
+// const add = function(apple = 5) {
+//     return apple + 10;
+// }
